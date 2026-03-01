72b666f221d7a27419ea2ddb32c0818c51f4dfef
¨Arruda¨ <joaopedroarruda323@gmail.com>
2026-03-01 20:38:28 +0000

    peguei a manha

---
+++ ex7volumeParalep.py
@@ -0,0 +1,7 @@
+comprimento = float(input('Digite o valor do comprimento:'))
+altura = float(input('Digite o valor da altura:'))
+largura = float(input('Digite o valor da largura:'))
+
+area = altura * comprimento * largura
+
+print('Area do paralelepipedo:', area, 'm')
